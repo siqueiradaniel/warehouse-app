4ec2c7e8960396b18ddfaaaa915d4db2bc1ea9da
siqueiradaniel <danielsiqueira111@gmail.com>
2023-11-11 13:19:12 -0300

    Test Done - Mostra galpões cadastrados

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
-describe 'Usuário abre a tela inicial' do 
-  it 'e vê o nome da app' do 
-    # Arrange 
+describe 'Usuário abre a tela inicial' do
+  it 'e vê o nome da app' do
+    # Arrange
 
-    # Act 
+    # Act
     visit('/')
 
-    # Assert 
+    # Assert
     expect(page).to have_content('Galpões & Estoque')
+  end
+
+  it 'e vê os galpões cadastrados' do
+    # Assent
+    Warehouse.create(name: 'Rio de Janeiro', code: 'RGU', city: 'Rio de Janeiro', area: '80000')
+    Warehouse.create(name: 'Maceio', code: 'MGU', city: 'Maceio', area: '60000')
+
+    # Act
+    visit('/')
+
+    # Assent
+    expect(page).to have_content('Rio de Janeiro')
+    expect(page).to have_content('Código: RGU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('80000 m2')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Código: MGU')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('60000 m2')
   end
 end
