5f5f2580f13f408b14e521c7a1ae61ecc58aec97
siqueiradaniel <danielsiqueira111@gmail.com>
2023-11-11 14:20:45 -0300

    Test Galpão view

---
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Usuário vê detalhe de galpão' do
+  it 'e vê informações adicionais' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100000,
+                     address: 'Avenida do aeroporto, 1000', cep: '15000-000',
+                     description: 'Galpão destinado para cargas internacionais')
+
+    # Act
+    visit('/')
+    click_on('Aeroporto SP')
+
+    # Assert
+    expect(page).to have_content('Nome: Aeroporto SP')
+    expect(page).to have_content('Código: GRU')
+    expect(page).to have_content('Cidade: Guarulhos')
+    expect(page).to have_content('Área: 100000')
+    expect(page).to have_content('Endereço: Avenida do aeroporto, 1000, CEP: 15000-000')
+    expect(page).to have_content('Galpão destinado para cargas internacionais')
+  end
+end
